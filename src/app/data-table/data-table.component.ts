e2be72811ae5bee13f2294f7e6f02332dc909383
xliu0227 <liuxiaolong@gwu.edu>
2019-08-18 18:10:28 -0400

    code clean up for client side paging and sorting

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -12,16 +12,13 @@ import {dashBoardEntry } from '../pageModel/dashBoardEntry';
 })
 export class DataTableComponent implements OnInit {
   page= new Page();
-  rows;//:Observable<any[]>;
-  //rows:Array<dashBoardEntry> = [];
+  rows;
   columns = [
       {prop: 'accumulated_turnover', name: 'Accumulated Turnover'},
       {prop: 'account_executive', name: 'Account Executive'},
       {prop: 'daily_turnover', name: 'Daily Turnover'}
   ]
   ngOnInit() {
-        //interval(5000).subscribe(() => {
-          
   }
  
 
@@ -34,44 +31,5 @@ export class DataTableComponent implements OnInit {
     this.rows = dataSer.getAllData();
     console.log(this.rows);
     });
-    
-    //this.rows = [...this.rows];
   } 
-
-
-
-  setPage(pageInfo){
-     console.log("trigger set page  " + pageInfo);
-    this.page.pageNumber = pageInfo.offset;
-    
-    // this.dataSer.getResult(this.page).
-    //   subscribe(pagedData => {
-    //     this.page = pagedData.page;
-    //     this.rows = pagedData.data;
-    //   });
-  }
-
-
-  onSort(event) {
-    // event was triggered, start sort sequence
-    console.log('Sort Event', event);
-    console.log(event.column.prop);
-    console.log(event.newValue);
-    //this.loading = true;
-    // emulate a server request with a timeout
-    // setTimeout(() => {
-    //   const rows = [...this.rows];
-    //   // this is only for demo purposes, normally
-    //   // your server would return the result for
-    //   // you and you would just set the rows prop
-    //   const sort = event.sorts[0];
-    //   rows.sort((a, b) => {
-    //     return a[sort.prop].localeCompare(b[sort.prop]) * (sort.dir === 'desc' ? -1 : 1);
-    //   });
-
-    //   this.rows = rows;
-    //   this.loading = false;
-    // }, 1000);
-  }
-
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { dashBoardEntry } from './pageModel/dashBoardEntry';
-import { PagedData } from './pageModel/paged-data';
-import { Page } from './pageModel/page';
-
 
 @Injectable({
   providedIn: 'root'
@@ -15,44 +10,8 @@ export class DataService {
   constructor(
     private http: HttpClient
   ) { }
-  
-  public getResult(page: Page): Observable<PagedData<dashBoardEntry>>{
-    const baseUrl = "http://rtq.chicheongweng.com:3000/securities";
-    const pagedData = new PagedData<dashBoardEntry>();
-    page.totalElements = this.items.length;
-    page.totalPages = page.totalElements / page.size;
-    let queryParam  = "";
-    const start = page.pageNumber * page.size;
-    const end = Math.min((start + page.size), page.totalElements);
-    for (let i = start; i < end; i++){
-        const jsonObj = this.items[i];
-        const entry = new dashBoardEntry(jsonObj.accumulated_turnover, jsonObj.account_executive, jsonObj.daily_turnover);
-        pagedData.data.push(entry);
-    }
-    pagedData.page = page;
-    return pagedData;
-  }
 
   getAllData(){
-    //this.items =[];
-    // this.http.get("http://rtq.chicheongweng.com:3000/securities").subscribe((res : any[])=>{
-    //   if(res){
-    //     for(let i = 0; i < res.length; i ++){
-    //       const jsonObj = res[i];
-
-    //       this.items.push(new dashBoardEntry(res[i].accumulated_turnover, res[i].account_executive, res[i].daily_turnover));
-    //       console.log(this.items[i]);
-    //     }
-    //     return this.items;
-    //   }
-    // });
-    // this.items = [];
-    //     this.http.get("http://rtq.chicheongweng.com:3000/securities").subscribe((res : any[])=>{
-    //     console.log(res);
-    //     this.items = res;
-    // });
-    // return this.items;
-
     this.http.get("http://rtq.chicheongweng.com:3000/securities").subscribe((res : any[])=>{
       console.log(res);
       this.items = res;
